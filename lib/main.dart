158f549384719799093b256e8a4ffb4494d6118d
Yashsharmacodes <yashsharma2752@gmail.com>
2023-10-03 13:20:13 +0530

    pre-final commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,64 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.teal[900],
         centerTitle: true,
       ),
+      body: const Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/yash.jpg'),
+            ),
+            Text(
+              'Yash Sharma',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Sourcesans',
+                  fontSize: 12.0,
+                  letterSpacing: 1.0),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.00),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 7533963976',
+                    style: TextStyle(color: Colors.teal),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.00),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'yashsharma2752@gmail.com',
+                    style: TextStyle(color: Colors.teal),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
